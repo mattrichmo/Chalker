5c6fff6afcd8757018cea45b316423b2f5cf8ff8
mattrichmo <matt.richmond3@gmail.com>
2024-01-08 00:29:21 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Chalker
+# Chalker 
+## Rainbow Logger for Your JSON Objects
+Recieves an an object as an argument and it will go through and chalk a new colour for each layer. Great for debugging and visualization. 
+
+
+### Example Output
+
+![Example Object](./assets/exampleObject.png)
+
+which is the result of:
+
+![Example Code](,/assets/exampleCode.png)
+
+
+### Add More Colours
+
+Colours are all formatted like this:
+
+```
+    {
+        name: 'Almond',
+        hex: '#EFDECD',
+        complimentary: `#CD9575`,
+        shades: {
+            dim: {
+                name: 'Almond (Dim)',
+                hex: '#D9D0C1'
+            },
+            bright: {
+                name: 'Almond (Bright)',
+                hex: '#F4E0E6'
+            },
+        }
+    },```
--- chalker.mjs
+++ chalker.mjs
@@ -1,9 +1,6 @@
 
 import chalk from 'chalk';
 
-
-
-
 const crayolaColours = [
     {
         name: 'Almond',
@@ -371,7 +368,6 @@ const colors = crayolaColours.map(color => chalk.hex(color.hex));
 const bgColors = crayolaColours.map(color => chalk.bgHex(color.hex));
 let colorIndex = 0;
 
-// Function to recursively print object with color formatting and indentation
 const chalker = (obj, depth = 0, indent = '') => {
     // Determine the color for the current level
     let colourIndex = depth % crayolaColours.length;
@@ -407,69 +403,3 @@ export default chalker;
 
 
 
-//example
-
-
-import chalker from './chalker.mjs';
-
-
-const testObject = {
-    Level1: { // this should be the bgHex.primary color and the text should be complimentary color
-        level1Text: 'Level 1 Text', //key would be the shades.dim color and value would be shades.bright color
-        level1Number: 1,
-        level1Boolean: true,
-        Level2: { // new bgHex.primary color and text should be complimentary color
-            level2Text: 'Level 2 Text', //key would be the shades.dim color and value would be shades.bright color
-            level2Number: 2,
-            level2Boolean: false,
-            Level3: {
-                level3Text: 'Level 3 Text',
-                level3Number: 3,
-                level3Boolean: true,
-                Level4: {
-                    level4Text: 'Level 4 Text',
-                    level4Number: 4,
-                    level4Boolean: false,
-                    Level5: {
-                        level5Text: 'Level 5 Text',
-                        level5Number: 5,
-                        level5Boolean: true,
-                        Level6: {
-                            level6Text: 'Level 6 Text',
-                            level6Number: 6,
-                            level6Boolean: false,
-                            Level7: {
-                                level7Text: 'Level 7 Text',
-                                level7Number: 7,
-                                level7Boolean: true,
-                                Level8: {
-                                    level8Text: 'Level 8 Text',
-                                    level8Number: 8,
-                                    level8Boolean: false,
-                                    Level9: {
-                                        level9Text: 'Level 9 Text',
-                                        level9Number: 9,
-                                        level9Boolean: true,
-                                        Level10: {
-                                            level10Text: 'Level 10 Text',
-                                            level10Number: 10,
-                                            level10Boolean: false
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-};
-
-const example = async () => {
-    console.log(`\n\n\n\n`)
-    await chalker(testObject); // passing the object to chalker
-    console.log(`\n\n\n\n`)
-}
-
-example();
